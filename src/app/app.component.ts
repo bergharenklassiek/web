c21e51a0494d866e1c35c1ae1532a3075c9d4f87
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-03-20 07:30:00 +0100

    Adding more stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { HeaderComponent } from './ui/components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaIconComponent],
+  imports: [RouterOutlet, FaIconComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
