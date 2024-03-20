076470ae8718c9c2b1888b59021f6349167f0538
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-03-20 18:47:02 +0100

    Start with header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,39 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ElementRef, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { HeaderComponent } from './ui/components/header/header.component';
 import { FooterComponent } from './ui/components/footer/footer.component';
 import { ContentService } from './core/services/content.service';
+import { MenuComponent } from './ui/components/menu/menu.component';
+import { ClickOutsideDirective } from './core/directives/click-outside.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaIconComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, FaIconComponent, HeaderComponent, FooterComponent, MenuComponent, ClickOutsideDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   faCoffee = faCoffee;
+  openMenu = false;
 
   constructor(private contentService: ContentService) {}
   
   ngOnInit(): void {
     this.contentService.loadData();
+  }
+
+  onOpenMenu(event: boolean) {
+    this.openMenu = event;
+  }
+
+  onCloseMenu(event: MouseEvent) {
+    if (typeof (event.target as any).parentElement?.className.includes === 'function' && 
+      !(event.target as any)?.parentElement?.className?.includes('app-header__menu')
+    ) {
+      this.openMenu = false;
+    }
   }
 }
--- src/app/ui/components/header/header.component.ts
+++ src/app/ui/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,4 +11,10 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent {
   menuIcon = faBars;
+  @Output() menuOpenEvent = new EventEmitter<boolean>();
+
+  onClickMenuIcon() {
+    this.menuOpenEvent.emit(true);
+  }
 }
+
