bb7cb91cb8df6d537d4e02e0e59ed148a56ba9ce
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-03-25 20:55:11 +0100

    Refactoring, restyling, footer

--- src/app/ui/components/footer/footer.component.ts
+++ src/app/ui/components/footer/footer.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { IconDefinition, faAt, faEuroSign, faFileLines, faLocationDot, faSignature } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FaIconComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+  iconsContactTypesMap: { [key: string]: IconDefinition } = {
+    'name': faSignature,
+    'kvk': faFileLines,
+    'account': faEuroSign,
+    'email': faAt,
+    'address': faLocationDot,
+  };
 
+  contactDetails = [
+    { type: 'name', value: 'Stichting Bergharen Klassiek' },
+    { type: 'kvk', value: '12345678' },
+    { type: 'account', value: 'NL12ABNA012345678' },
+    { type: 'email', value: 'bergharenklassiek@outlook.com' },
+    { type: 'address', value: 'Veldsestraat 10, Bergharen' }
+  ];
 }
