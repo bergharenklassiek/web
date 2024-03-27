1ec766d703f65fc13d31acbaa2abfa1ee1d15a88
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-03-27 20:29:13 +0100

    Optimizations

--- src/app/ui/components/event-card/event-card.component.ts
+++ src/app/ui/components/event-card/event-card.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Event } from '../../../core/models/event';
 import { StoryBlokUrlPipe } from '../../../core/pipes/story-blok-url.pipe';
-import { DatePipe } from '@angular/common';
+import { DatePipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Story } from '../../../core/models/story';
 
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [ StoryBlokUrlPipe, DatePipe, RouterLink ],
+  imports: [ StoryBlokUrlPipe, DatePipe, RouterLink, NgOptimizedImage ],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.scss'
 })
 export class EventCardComponent {
   @Input() story!: Story<Event>;
+  @Input() cardIndex!: number;
 }
