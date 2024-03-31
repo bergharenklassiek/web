52c2fdb3d6982fd791691178388ec81907030222
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-03-31 11:21:48 +0200

    Resize images

--- src/app/core/pipes/story-blok-url.pipe.ts
+++ src/app/core/pipes/story-blok-url.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StoryBlokUrlPipe implements PipeTransform {
 
-  transform(url: string): string {
-    return url.replace('//a.storyblok.com', '//a2.storyblok.com');
+  transform(url: string, size?: number): string {
+    return `${url.replace('//a.storyblok.com', '//a2.storyblok.com')}${size ? `/m/${size}x0` : ''}`;
   }
 }
