d35b2766106956ca438f61ff1b99da348dd60bdc
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-04-05 14:42:06 +0200

    Add Contact Page

--- src/app/ui/components/footer/footer.component.ts
+++ src/app/ui/components/footer/footer.component.ts
@@ -1,24 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
-import { IconDefinition, faAt, faEuroSign, faFileLines, faLocationDot, faSignature } from '@fortawesome/free-solid-svg-icons';
-import { ContentService } from '../../../core/services/content.service';
+import { ContactDetailsComponent } from '../contact-details/contact-details.component';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [FaIconComponent],
+  imports: [FaIconComponent,ContactDetailsComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
-export class FooterComponent {
-  iconsContactTypesMap: { [key: string]: IconDefinition } = {
-    'name': faSignature,
-    'kvk': faFileLines,
-    'bankaccount': faEuroSign,
-    'email': faAt,
-    'address': faLocationDot,
-  };
-  contactList = this.contentService.contactList;
-
-  constructor(private contentService: ContentService) {}
-}
+export class FooterComponent {}
