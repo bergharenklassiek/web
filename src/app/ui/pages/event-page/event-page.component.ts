0e31d81b3c9d5cc923f3cc6052563aa502947199
Jan Joosse <129174800+rzjajo@users.noreply.github.com>
2024-04-17 10:22:02 +0200

    Remove unnecessary imports for CommonModule

--- src/app/ui/pages/event-page/event-page.component.ts
+++ src/app/ui/pages/event-page/event-page.component.ts
@@ -1,7 +1,6 @@
 import { AfterViewInit, CUSTOM_ELEMENTS_SCHEMA, ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 import { RichTextComponent } from '../../components/rich-text/rich-text.component';
 import { ActivatedRoute } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { StoryBlokUrlPipe } from '../../../core/pipes/story-blok-url.pipe';
 import { ContentService } from '../../../core/services/content.service';
 import { BreakpointObserver, LayoutModule } from '@angular/cdk/layout';
@@ -12,7 +11,7 @@ import { AppDatePipe } from '../../../core/pipes/app-date.pipe';
   selector: 'app-event-page',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [RichTextComponent, CommonModule, StoryBlokUrlPipe, LayoutModule, AppDatePipe ],
+  imports: [RichTextComponent, StoryBlokUrlPipe, LayoutModule, AppDatePipe ],
   templateUrl: './event-page.component.html',
   styleUrl: './event-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
--- src/app/ui/pages/home-page/home-page.component.ts
+++ src/app/ui/pages/home-page/home-page.component.ts
@@ -1,14 +1,13 @@
 import { ChangeDetectionStrategy, Component, afterNextRender } from '@angular/core';
 import { ContentService } from '../../../core/services/content.service';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
-import { CommonModule } from '@angular/common';
 import { RichTextComponent } from '../../components/rich-text/rich-text.component';
 import { scrollLeftKey, scrollListKey } from '../../../app.config';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [EventCardComponent, CommonModule, RichTextComponent],
+  imports: [EventCardComponent, RichTextComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
