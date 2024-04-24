82f5232b04419e87a6df02f4484ecce0c1bf94c6
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-04-24 23:48:02 +0200

    Fix flickering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, afterNextRender } from '@angular/core';
+import { Component, Inject, afterNextRender } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +6,8 @@ import { HeaderComponent } from './ui/components/header/header.component';
 import { FooterComponent } from './ui/components/footer/footer.component';
 import { ContentService } from './core/services/content.service';
 import { scrollLeftKey } from './app.config';
+import { DOCUMENT } from '@angular/common';
+import { config, dom } from "@fortawesome/fontawesome-svg-core";
 
 @Component({
   selector: 'app-root',
@@ -18,8 +20,14 @@ export class AppComponent {
   faCoffee = faCoffee;
   openMenu = false;
 
-  constructor(private contentService: ContentService) {
+  constructor(private contentService: ContentService, @Inject(DOCUMENT) private document: Document) {
     afterNextRender(() => localStorage.removeItem(scrollLeftKey));
+
+    config.autoAddCss = false;
+    let head = this.document.getElementsByTagName("head")[0];
+    let styleNode = this.document.createElement("style");
+    styleNode.innerHTML = dom.css(); // grab FA's CSS
+    head.appendChild(styleNode);
   }
   
   ngOnInit(): void {
