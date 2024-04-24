3426e3f770e0c2982f2370973475c816c46622d8
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-04-24 20:44:53 +0200

    Add Honeycomb open-telemetry

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register as registerSwiperElements } from 'swiper/element/bundle';
 
+import './honeycomb';
+
 registerSwiperElements();
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
