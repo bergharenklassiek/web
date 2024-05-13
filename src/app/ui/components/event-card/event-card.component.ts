921e4fefb1d80810789181c20c63beb93f29225e
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-05-13 20:56:35 +0200

    Remove use of ngOptimizedImage

--- src/app/ui/components/event-card/event-card.component.ts
+++ src/app/ui/components/event-card/event-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Event } from '../../../core/models/event';
 import { StoryBlokUrlPipe } from '../../../core/pipes/story-blok-url.pipe';
-import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Story } from '../../../core/models/story';
 import { AppDatePipe } from '../../../core/pipes/app-date.pipe';
@@ -9,7 +9,7 @@ import { AppDatePipe } from '../../../core/pipes/app-date.pipe';
 @Component({
   selector: 'app-event-card',
   standalone: true,
-  imports: [ StoryBlokUrlPipe, DatePipe, RouterModule, NgOptimizedImage, AppDatePipe ],
+  imports: [ StoryBlokUrlPipe, DatePipe, RouterModule, AppDatePipe ],
   templateUrl: './event-card.component.html',
   styleUrl: './event-card.component.scss'
 })
