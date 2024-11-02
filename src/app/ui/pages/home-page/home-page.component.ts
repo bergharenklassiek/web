b8c9c9c8f6ae5831ff5cec46228f4667e6609e06
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-11-02 13:47:13 +0100

    Refactoring styling

--- src/app/ui/pages/home-page/home-page.component.ts
+++ src/app/ui/pages/home-page/home-page.component.ts
@@ -3,12 +3,12 @@ import { ContentService } from '../../../core/services/content.service';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
 import { RichTextComponent } from '../../components/rich-text/rich-text.component';
 import { scrollLeftKey, scrollListKey } from '../../../app.config';
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [EventCardComponent, RichTextComponent, NgFor],
+  imports: [EventCardComponent, RichTextComponent, NgFor, NgClass],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
