19f5bd8f51b2c0f8222ed3783c240718ca4ad319
janjoosse <24371035+janjoosse@users.noreply.github.com>
2024-11-09 12:18:40 +0100

    Refactoring

--- src/app/core/pipes/app-date.pipe.ts
+++ src/app/core/pipes/app-date.pipe.ts
@@ -8,9 +8,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AppDatePipe implements PipeTransform {
   constructor(private datePipe: DatePipe) {}
 
-  transform(date: string): string | null {
+  transform(date: string, format: 'month' | 'year' = 'month'): string | null {
     return this.datePipe.transform(
       date,
+      format === 'year' ? 'EE d MMMM y' :
       date.split('-')[0] === new Date().getFullYear().toString() 
         ? 'EE d MMMM HH:mm'
         : 'EE d MMMM y HH:mm'
