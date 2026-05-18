73b10b67e5cb6808abce5c50f4376e4ea5045c39
janjoosse <24371035+janjoosse@users.noreply.github.com>
2026-05-18 20:56:40 +0200

    Chore/refactor header (#16)
    * Refactor header
    
    * Cleanup

--- src/app/ui/pages/home-page/home-page.component.ts
+++ src/app/ui/pages/home-page/home-page.component.ts
@@ -1,39 +1,19 @@
-import { ChangeDetectionStrategy, Component, afterNextRender } from '@angular/core';
-import { ContentService } from '../../../core/services/content.service';
+import { Component, inject } from '@angular/core';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
 import { RichTextComponent } from '../../components/rich-text/rich-text.component';
-import { scrollLeftKey, scrollListKey } from '../../../app.config';
-import { AsyncPipe, NgClass } from '@angular/common';
-import { Observable } from 'rxjs';
-import { Event } from '../../../core/models/event';
-import { Story } from '../../../core/models/story';
+import { AsyncPipe } from '@angular/common';
 import { select, Store } from '@ngrx/store';
 import { selectEvents, selectHomePage } from '../../../core/store/content.selectors';
-import { HomePage } from '../../../core/models/home-page';
 
 @Component({
     selector: 'app-home-page',
-    standalone: true,
-    imports: [EventCardComponent, RichTextComponent, NgClass, AsyncPipe],
+    imports: [EventCardComponent, RichTextComponent, AsyncPipe],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePageComponent {
-  homePage$?: Observable<Story<HomePage> | undefined>;
-  events$?: Observable<Story<Event>[] | undefined>;
-  
-  constructor(private store: Store) {
-    this.events$ = this.store.pipe(select(selectEvents(false)));
-    this.homePage$ = this.store.pipe(select(selectHomePage));
-    
-    afterNextRender(() => {
-      const scrollLeft = localStorage?.getItem(scrollLeftKey);
-      document.getElementById(scrollListKey)?.scrollTo({ left: parseInt(scrollLeft ?? '0') });
-    });
-  }
-  
-  onScroll(_: any) {
-    localStorage.setItem(scrollLeftKey, (document.getElementById(scrollListKey)?.scrollLeft ?? 0).toString());
-  }
+  private readonly store = inject(Store);
+
+  events$ = this.store.pipe(select(selectEvents(false)));
+  homePage$ = this.store.pipe(select(selectHomePage));
 }
