193c9f10de768de43e730789921814c40f33fd3f
janjoosse <24371035+janjoosse@users.noreply.github.com>
2026-05-23 15:43:40 +0200

    Chore/refactor events (#19)
    * Start with HttpResource
    
    * refactor events

--- src/app/ui/pages/home-page/home-page.component.ts
+++ src/app/ui/pages/home-page/home-page.component.ts
@@ -1,14 +1,12 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { EventCardComponent } from '../../components/event-card/event-card.component';
 import { RichTextComponent } from '../../components/rich-text/rich-text.component';
-import { AsyncPipe } from '@angular/common';
-import { select, Store } from '@ngrx/store';
-import { selectEvents } from '../../../core/store/content.selectors';
+import { Store } from '@ngrx/store';
 import { ContentService } from '../../../core/services/content.service';
 
 @Component({
     selector: 'app-home-page',
-    imports: [EventCardComponent, RichTextComponent, AsyncPipe],
+    imports: [EventCardComponent, RichTextComponent],
     templateUrl: './home-page.component.html',
     styleUrl: './home-page.component.scss',
 })
@@ -16,6 +14,6 @@ export class HomePageComponent {
   private readonly store = inject(Store);
   private readonly contentService = inject(ContentService);
 
-  events$ = this.store.pipe(select(selectEvents(false)));
   homePage = this.contentService.homePage;
+  events = this.contentService.events;
 }
